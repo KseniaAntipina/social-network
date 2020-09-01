9a209ef2169a1178a2256f5c536684d61664f14f
Kseniya Antipina <karinpark@mail.ru>
2020-09-01 13:25:12 +0300

    edited mdtp in users container

---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+import preloader from "../../../assets/images/preloader.svg";
+import React from "react";
+
+
+let Preloader = () => {
+    return (
+        <img src={preloader}/>
+    )
+}
+
+export default Preloader;
